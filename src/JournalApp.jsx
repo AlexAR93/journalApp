5ecf968ccdd22ff242fe7b70b9447113ad604aff
AlexAR93 <AlexAR93>
2024-05-28 11:37:54 -0300

    repaired routes

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,7 @@
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './theme';
 
-export const JournalApp = () => {
+export const JournalApp =()=> {
   return (
     <AppTheme>
         <AppRouter />
